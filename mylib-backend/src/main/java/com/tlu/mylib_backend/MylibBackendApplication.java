600961df1d43bfacdc54fb5707844872a4633e9c
tammuahe <tammuahe2004@gmail.com>
2025-12-08 16:45:59 +0700

    initial commit

---
+++ mylib-backend/src/main/java/com/tlu/mylib_backend/MylibBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tlu.mylib_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylibBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylibBackendApplication.class, args);
+	}
+
+}
---
+++ mylib-backend/src/test/java/com/tlu/mylib_backend/MylibBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlu.mylib_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylibBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
