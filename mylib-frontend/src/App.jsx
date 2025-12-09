4cdee7ccae4a7590830ca4d3df5c92c29fc8825b
hoangcuong281 <ngohoangcuong28@gmail.com>
2025-12-09 11:23:39 +0700

    test 2nd

--- mylib-frontend/src/App.jsx
+++ mylib-frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> LIB
+          Edit <code>src/App.jsx</code> LIBRARY
         </p>
       </div>
       <p className="read-the-docs">
