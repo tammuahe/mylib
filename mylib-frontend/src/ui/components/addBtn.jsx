518fa53651e12625a90dc861e95972cb0c4881e3
hoangcuong <ngohoangcuong28@gmail.com>
2025-12-15 21:24:08 +0700

    feat: add basic book management UI - install lucide-react package - create components folder - add AddButton and SearchBar components - implement add dialog and book list on manage book page

---
+++ mylib-frontend/src/ui/components/addBtn.jsx
@@ -0,0 +1,13 @@
+const AddBtn = ({showDialog, setShowDialog}) => {
+    return (
+        <button 
+            className="flex items-center rounded-lg bg-[#7cd97f] px-3 py-1 text-black font-bold gap-6 cursor-pointer hover:bg-[#6bc86f] transition"
+            onClick={() => setShowDialog(!showDialog)}
+        >
+            <span>Thêm</span>
+            <span className="text-2xl">+</span>
+        </button>
+    );
+}
+ 
+export default AddBtn;
---
+++ mylib-frontend/src/ui/components/searchBar.jsx
@@ -0,0 +1,18 @@
+import { Search, Filter, X } from "lucide-react"
+
+const SearchBar = ({placeHolder}) => {
+    return ( 
+        <div className="flex gap-2 px-4 py-2 bg-(--black) rounded-xl">
+            <input
+                type="text"
+                placeholder={placeHolder}
+                size={placeHolder.length - 10}
+            />
+            <button>
+                <Search className="w-5 h-5 opacity-50 cursor-pointer"/>
+            </button>
+        </div>
+    );
+}
+ 
+export default SearchBar;
