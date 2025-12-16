e3b4c49837808b0f306bb2176c566399d45ca8bb
tammuahe <tammuahe2004@gmail.com>
2025-12-16 16:21:37 +0700

    feat(backend): add search by title or author in repository layer

--- mylib-backend/src/main/java/com/tlu/mylib_backend/repository/BookRepository.java
+++ mylib-backend/src/main/java/com/tlu/mylib_backend/repository/BookRepository.java
@@ -1,9 +1,18 @@
 package com.tlu.mylib_backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.tlu.mylib_backend.entity.Book;
 
-public interface BookRepository extends JpaRepository<Book, Long>{
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @Query("SELECT DISTINCT b FROM Book b JOIN b.authors a " +
+            "WHERE LOWER(b.title) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "OR LOWER(a.name) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Book> searchByTitleOrAuthor(@Param("keyword") String keyword);
 
 }
